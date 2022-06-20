4ca8d2a70be093b515801a3964aa81d51fbd2c05
umbrelluck <yarko.umbrelluck@gmail.com>
2022-06-20 23:42:08 +0300

    f

---
+++ Frontend/web-exam/src/Home.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+
+export class Home extends Component{
+
+    render(){
+        return(
+            <div className="mt-5 d-flex justify-content-left">
+                <p>This is EMS home page.</p>
+            </div>
+        )
+    }
+}
